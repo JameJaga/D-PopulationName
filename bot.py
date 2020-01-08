4a622a111c6a35e76ce0a547cb871065eccf0ec7
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-01-08 17:03:28 +0900

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+
+TOKEN = os.environ.get("DISCORD_TOKEN")
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def discord.on_group_join
+    name = "ジャガの部屋"
+    population = member_count
+    Gname = name + pupulation
+    guild = client.get_guild(662153006787199046)
+    await guild.edit(name=Gname)
+async def discord.on_group_remove
+    name = "ジャガの部屋"
+    population = member_count
+    Gname = name + pupulation
+    guild = client.get_guild(662153006787199046)
+    await guild.edit(name=Gname)
+    
+client.run(TOKEN)
