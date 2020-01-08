ddc894c7709adbb91467c88823f45a9e653887a6
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-01-08 16:48:25 +0900

    Update source.py

--- src/source.py
+++ src/source.py
@@ -11,10 +11,14 @@ async def discord.on_group_join
     name = "ジャガの部屋"
     population = member_count
     Gname = name + pupulation
-    await edit.name(Gname)
+    guild = client.get_guild(662153006787199046)
+    await guild.edit(name=Gname)
 async def discord.on_group_remove
     name = "ジャガの部屋"
     population = member_count
     Gname = name + pupulation
-    await edit.name(Gname)
+    guild = client.get_guild(662153006787199046)
+    await guild.edit(name=Gname)
+    
+    
 client.run('NjY0MzYxNDk1OTQxMDg3MjQy.XhV9lg.cqfuulHf95pO_qo_bI7WKIE7TGk')
