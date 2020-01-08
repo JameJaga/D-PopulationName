dd7a01058a286e6fae8ac93c0a4c93c7c79f9c5f
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-01-08 17:15:46 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,13 +9,13 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
-async def discord.on_group_join
+async def discord.on_group_join(channel, user)
     name = "ジャガの部屋"
     population = member_count
     Gname = name + pupulation
     guild = client.get_guild(662153006787199046)
     await guild.edit(name=Gname)
-async def discord.on_group_remove
+async def discord.on_group_remove(channel, user)
     name = "ジャガの部屋"
     population = member_count
     Gname = name + pupulation
