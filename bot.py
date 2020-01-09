dacd15faef8dd6cf3d2c81a98a688189fb40452c
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-01-09 17:41:25 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,13 +12,13 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     name = 'ジャガの部屋'
-    population = await member_count
+    await member_count(population)
     Gname = name + pupulation + 'Members'
     guild = client.get_guild(662153006787199046)
     await guild.edit(name=Gname)
 async def on_member_remove(member):
     name = 'ジャガの部屋'
-    member_count(population)
+    await member_count(population)
     Gname = name + pupulation + 'Members'
     guild = client.get_guild(662153006787199046)
     await guild.edit(name=Gname)
