21e500d6245a074b36de960a38d81a18e8d3a19c
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-01-11 14:50:40 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,20 +14,57 @@ async def on_ready():
 async def on_message(message):            
     from discord.ext import commands
     guild = message.guild
-    member_count = guild.member_count   
+    member_count = guild.member_count 
+    user_count = sum(1 for member in guild.members if not member.bot)
+    bot_count = sum(1 for member in guild.members if member.bot)  
+    TextC_count = len(guild.text_channels)
+    VoiceC_count = len(guild.voice_channels)
+    Role_count = len(guild.roles)
+    Region = guild.region
+    
+    #参加人数
     ChannelName = f'Members:{str(member_count)}'
     VoiceChannel = client.get_channel(665387061750136841)
     await VoiceChannel.edit(name=ChannelName)
+    #Openの方
     VoiceChannel = client.get_channel(665387771736621056)
     await VoiceChannel.edit(name=ChannelName)
-    user_count = sum(1 for member in guild.members if not member.bot)
+    
+    #ユーザー数
     ChannelName = f'Humans:{str(user_count)}'
     VoiceChannel = client.get_channel(665387186660573206)
     await VoiceChannel.edit(name=ChannelName) 
-    bot_count = sum(1 for member in guild.members if member.bot)  
+    
+    #ボット数
     ChannelName = f'Bots:{str(bot_count)}'
     VoiceChannel = client.get_channel(665387101164142622)
     await VoiceChannel.edit(name=ChannelName) 
+    
+    #テキストチャンネル数
+    ChannelName = f'TextChannles:{str(TextC_count)}'
+    VoiceChannel = client.get_channel(665387508586119198)
+    await VoiceChannel.edit(name=ChannelName) 
+    
+    #ボイスチャンネル数
+    ChannelName = f'VoiceChannels:{str(TextC_count)}'
+    VoiceChannel = client.get_channel(665387575963549741)
+    await VoiceChannel.edit(name=ChannelName)
+    
+    #チャンネル総数
+    ChannelName = f'AllChannels:{str(TextC_count + VoiceC_count)}'
+    VoiceChannel = client.get_channel(665387403745165333)
+    await VoiceChannel.edit(name=ChannelName)
+    
+    #ロール総数
+    ChannelName = f'Roles:{str(Role_count)}'
+    VoiceChannel = client.get_channel(665387612453994496)
+    await VoiceChannel.edit(name=ChannelName)
+    
+    #Region
+    ChannelName = 'Region:' + Region
+    VoiceChannel = client.get_channel(665387642925613066)
+    await VoiceChannel.edit(name=ChannelName)
+    
     num = len(guild.members)
     GuildName = f'ジャガの部屋 ＋{str(num)}Members'
     await guild.edit(name=GuildName)
